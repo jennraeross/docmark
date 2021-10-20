9366602f9cff6cf5dcdb6fab458c1a56d35548ec
Jennifer Roylance <jenn.rae.ross@gmail.com>
2021-10-20 14:44:52 -0600

    Clean up code

--- docmark.js
+++ docmark.js
@@ -14,6 +14,7 @@ var md = require('markdown-it')();
 // open (opens files in native system)
 const open = require('open');
 
+// Beginning of default boilerplate for generated html file
 var boilerplateHeaderOpening = `
 <!DOCTYPE html>
 <html lang="en">
@@ -24,36 +25,46 @@ var boilerplateHeaderOpening = `
     <meta name="description" content="" />
     <script src="https://unpkg.com/pagedjs/dist/paged.polyfill.js"></script>`;
 
+// closing part of header boilerplate (to allow the stylesheet to be inserted later)    
 var boilerplateHeaderClosing = `
   </head>
   <body>`;
 
+// Boilerplate for the end of the html file
 var boilerplateEnd = `
   </body>
   </html>`
 
 //* Main Code -----------------------------
 
+// Use commander to create commandline arguments
 program
   .requiredOption('-i, --input <files>', 'markdown files to convert')
   .option('-s, --style <sheet>', 'stylesheet to use in conversion');
-
 program.parse(process.argv);
 
+// Variable containing options chosen by user
 const options = program.opts();
 
+// Add stylesheet, if provided
 if (options.style) {
   boilerplateHeaderClosing = '<link rel="stylesheet" type="text/css" href="' + options.style + '" />' + boilerplateHeaderClosing;
 }
 
+// Create HTML file and open in browser
 fs.readFile(options.input, 'utf-8', function (err, data) {
   if (err) {
     return console.log(err);
   }
+  
+  // Combine boilerplate with generated html
   var results = boilerplateHeaderOpening + boilerplateHeaderClosing + md.render(data) + boilerplateEnd;
-  console.log(results);
+  
+  // Write to file called output.html
   fs.writeFile('output.html', results, function (err) {
     if (err) return console.log(err);
+
+    // TODO: Open file in browser
     open('output.html');
   })
 });
